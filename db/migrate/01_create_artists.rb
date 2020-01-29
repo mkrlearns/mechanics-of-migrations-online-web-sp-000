54244274f1852df32253f955f880a175e1c5d691
ryan m <contact@ryanmeek.com>
2020-01-29 12:08:51 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,10 +1,10 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
-  def up
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
   end
-
-  def down
-  end
-
-  def change() create_table :artists { |t| } end
-
 end
